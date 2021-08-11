a4b43927e6964ac92ad6a8c9eecf5b107c2e91d6
Lbb <123@Lbb.com>
2021-08-11 16:12:29 +0800

    first commit

---
+++ src/main/java/com/atlbb/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atlbb.git;
+
+/**
+ * @author LiangQingBin
+ * @version 1.00
+ * @ClassName GitTest
+ * @Description TODO
+ * @time 2021/8/11 16:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
